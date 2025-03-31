9ae9de0f2bf052acf778e9bde8700a8cda70ce31
amritha-sabu <amritha.g.sabu@gmail.com>
2025-03-31 23:46:53 +1100

    Building the Landing Page

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -1,3 +1,11 @@
-const LandingPage = () => {};
+import Hero from "./Hero";
+
+const LandingPage = () => {
+    return (
+        <div className="landing-page">
+            <Hero />
+        </div>
+    );
+};
 
 export default LandingPage;
